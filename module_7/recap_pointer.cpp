3730b7ac018ee1c7bf3a47eb62238b552ba60cba
zishan344 <zishanahmed344@gmail.com>
2024-06-26 19:56:11 +0600

    write small pointer arithmatic operation

--- module_7/recap_pointer.cpp
+++ module_7/recap_pointer.cpp
@@ -4,12 +4,19 @@ using namespace std;
 int main()
 {
   // initial tested in pointer
-  int a = 10;
-  int *ptr = &a;
-  cout << "value of a: " << a << endl;
-  cout << "address of a: " << &a << endl;
-  cout << "value of a ptr: " << ptr << endl;
-  cout << "value of pointed by ptr: " << *ptr << endl;
+  /*  int a = 10;
+   int *ptr = &a;
+   cout << "value of a: " << a << endl;
+   cout << "address of a: " << &a << endl;
+   cout << "value of a ptr: " << ptr << endl;
+   cout << "value of pointed by ptr: " << *ptr << endl; */
+
+  // pointer arithmetic
+  int arr[3] = {1, 2, 3};
+  int *ptr2 = arr;
+  cout << *ptr2 << endl;
+  ptr2++;
+  cout << *ptr2 << endl;
 
   return 0;
 }
