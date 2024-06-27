fe4a943b708a23ceeebdf9776ca9eb090921ce65
zishan344 <zishanahmed344@gmail.com>
2024-06-27 12:44:47 +0600

    complete insert_at_tail function concept

---
+++ module_7/insert_at_tail.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+  int val;
+  Node *next;
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+// initial function for insert a tail;
+/* void insert_at_tail(Node *head, Node *val)
+{
+  Node *newNode = new Node(val->val);
+
+  Node *temp = head;
+  // cout << temp << " " << endl;
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+};
+ */
+
+// efficient function for insert a tail
+
+void insert_at_tail(Node *&head, int val)
+{
+  Node *newNode = new Node(val);
+
+  if (head == NULL)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  cout << temp << " old temp" << endl;
+  int a = 0;
+  while (temp->next != NULL)
+  {
+    a += 1;
+    cout << temp->next << " new temp value" << endl;
+    temp = temp->next;
+  };
+
+  temp->next = newNode;
+  cout << a << " count " << endl;
+}
+
+// exercise different approach
+
+/* void insert_at_tail(Node **head, int val)
+{
+  Node *newNode = new Node(val);
+  cout << "head address " << head << endl;
+  cout << "head value " << *head << endl;
+  if (*head == NULL)
+  {
+    *head = newNode;
+  }
+  cout << "head value " << head << endl;
+}
+ */
+// printed linked list
+void print_linked_list(Node *head)
+{
+  if (head == NULL)
+  {
+    cout << endl
+         << "head is NULL" << endl;
+    return;
+  }
+  Node *tmp = head;
+  while (tmp != NULL)
+  {
+    cout << tmp->val << " ";
+    tmp = tmp->next;
+  }
+};
+
+int main()
+{
+  // Node *head = NULL;
+  Node *head = NULL;
+  /*  Node *a = new Node(20);
+   Node *b = new Node(30);
+   head->next = a;
+   insert_at_tail(head, b);*/
+  // int a = NULL;
+
+  // insert_at_tail(&head, 10);
+  insert_at_tail(head, 10);
+  insert_at_tail(head, 20);
+  insert_at_tail(head, 30);
+  insert_at_tail(head, 40);
+
+  print_linked_list(head);
+  cout
+      << endl;
+
+  return 0;
+}
--- module_7/test.cpp
+++ module_7/test.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void modify(int &a)
+{
+  cout << a << " " << endl;
+  a = 0;
+};
 int main()
 {
-  cout << "Hello world";
+  int a = 10;
+  modify(a);
+  cout << a;
   return 0;
 }
